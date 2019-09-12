9de2ba0b248d55f967d32f0aeceac8738d93b028
baikaifa <1956841815@qq.com>
2019-09-12 20:08:24 +0800

    调用useEffect

--- react-hook/src/App.js
+++ react-hook/src/App.js
@@ -1,29 +1,33 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-const App = (props)=> {
+const App = props => {
   const [count, setCount] = useState(0);
-  
-  useEffect(()=>{
-    document.title=`you clicked ${count} times`;
-  })
+  const [name, setName] = useState("rails365");
+  useEffect(() => {
+      console.log(`render ${count}`);
+    document.title = `you clicked ${count} times`;
+  },[count]);//当count变化的时候再执行里面的内容 当为空数组时候除了第一次就不再执行useEffect
+  useEffect(() => {
+    console.log("render name");
+  });
   //在以下两个情况调用useEffect
-//   componentDidMount(){
-//     document.title=`you clicked ${this.StaticRange.count} times`
-// }
+  //   componentDidMount(){
+  //     document.title=`you clicked ${this.StaticRange.count} times`
+  // }
 
-// componentDidUpdate(){
-//   document.title=`you clicked ${this.StaticRange.count} times`
-// }
-
+  // componentDidUpdate(){
+  //   document.title=`you clicked ${this.StaticRange.count} times`
+  // }
 
   return (
     <div>
-      <p>you clicked {count} times</p>
-      <button onClick={()=>setCount(count+1)}>
-          CLick me
-      </button>
+      <div>
+        <p>you clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>CLick me</button>
+      </div>
+      <button onClick={() => setName("")}>setName</button>
     </div>
-  );  
+  );
 };
 
 export default App;
