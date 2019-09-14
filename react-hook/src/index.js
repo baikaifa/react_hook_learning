dc85ef7bef54670c38f8376e01cbe944b20c5f80
baikaifa <1956841815@qq.com>
2019-09-14 19:56:30 +0800

    useMemo解决子组件重复执行问题

--- react-hook/src/index.js
+++ react-hook/src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Example from './Example6/Example6'
+import Example from './Example7'
 ReactDOM.render(<Example />, document.getElementById('root'));
