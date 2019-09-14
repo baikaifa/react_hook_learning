bb4a9fef5208cd75e1001be0f211f5e049de6237
baikaifa <1956841815@qq.com>
2019-09-14 12:05:26 +0800

    useReducer代替redux小案例

--- react-hook/src/index.js
+++ react-hook/src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Example from './Example5'
+import Example from './Example6/Example6'
 ReactDOM.render(<Example />, document.getElementById('root'));
