731152e9072a65e6bd9f537f82b8f09e90da9b75
baikaifa <1956841815@qq.com>
2019-09-14 21:41:30 +0800

    useRef获取DOM和保存变量

--- react-hook/src/Example7.js
+++ react-hook/src/Example7.js
@@ -1,4 +1,4 @@
-import React, { useState,useMemo } from "react";
+import React, { useState, useMemo } from "react";
 function Example7() {
   const [xiaohong, setXiaohong] = useState("小红在待客状态");
   const [zhiling, setZhiling] = useState("志玲在待客状态");
@@ -13,7 +13,7 @@ function Example7() {
       </button>
       <button
         onClick={() => {
-            setZhiling(new Date().getTime() + "志玲向我们走来");
+          setZhiling(new Date().getTime() + "志玲向我们走来");
         }}
       >
         志玲
@@ -22,15 +22,16 @@ function Example7() {
     </>
   );
 }
-
 function ChildComponent({ name, children }) {
+  // props.children
+  // 使用自定义组件的时候，可以在其中嵌套 JSX 结构。嵌套的结构在组件内部都可以通过 props.children 获取到.
+  // props.children其实是个数组，React.js 就是把我们嵌套的 JSX 元素一个个都放到数组当中，然后通过 props.children 传给了 自定义的组件。
+  //children的意思是父组件往该组件标签里内插入的内容
   function changeXiaohong() {
     console.log("她来了，她来了，小红向我们走来");
     return name + ",小红向我们走来了";
   }
-
-  const actionXiaohong=useMemo(() => changeXiaohong(name),[name])//当name发生变化的时候执行 
-
+  const actionXiaohong = useMemo(() => changeXiaohong(name), [name]); //当name发生变化的时候执行
   return (
     <>
       <div>{actionXiaohong}</div>
@@ -38,4 +39,4 @@ function ChildComponent({ name, children }) {
     </>
   );
 }
-export default Example7
+export default Example7;
--- react-hook/src/index.js
+++ react-hook/src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Example from './Example7'
+import Example from './Example8'
 ReactDOM.render(<Example />, document.getElementById('root'));
